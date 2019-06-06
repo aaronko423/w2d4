f19aef94a2f92f63b49535a87433cd3658353f11
Aaron Ko <aaronkotzehin@gmail.com>
2019-06-06 14:57:21 -0400

    add files from w2d4

---
+++ controller.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import time
+
+from models import User
+
+
+def infinite_loop():
+    #username = view.login_menu()
+    username = input("Your app, username: ")
+    password = input("Your app, password: ")
+   
+    
+    ####
+    # FIXME Make it more dynamic
+    user = User(username, password)
+    stored_username = user.fetch_username()
+    stored_password = user.fetch_password()
+
+
+    if username == stored_username:
+        if password == stored_password:
+            return "successful Login"
+        return "catch-all type-thing"
+
+if __name__ == "__main__":
+    print(infinite_loop())
---
+++ schema.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import sqlite3
+
+class Schema:
+
+    def __init__(self):
+        self.connection = sqlite3.connect("trade.db")
+        self.cursor = self.connection.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        if self.connection:
+            if self.cursor:
+                self.connection.commit()
+                self.cursor.close()
+            self.connection.close()
+
+    def create_table(self, table_name):
+        self.cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
+        self.cursor.execute(f'''
+            CREATE TABLE {table_name}(
+            pk INTEGER PRIMARY KEY AUTOINCREMENT);''')
+
+    def modify_table(self, table_name, column_name, column_type):
+        self.cursor.execute(
+            f'''ALTER TABLE {table_name}
+                ADD COLUMN {column_name} {column_type};''')
+
+if __name__ == "__main__":
+    #Schema().create_table("user_info")
+    Schema().modify_table("user_info", "password", "VARCHAR")
+    Schema().modify_table("user_info", "balance", "FLOAT")
